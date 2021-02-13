170779240e17d449f6587419d6216ca590f898d3
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-13 17:40:32 +0100

    cleanup: tmp dir

--- install.js
+++ install.js
@@ -1,18 +1,18 @@
-var get = require('simple-get')
+const get = require('simple-get')
 // var crypto = require('crypto')
-var os = require('os')
-var fs = require('fs')
-var stream = require('stream')
-var path = require('path')
-var unzipper = require('unzipper')
-var version = require("./package.json").version
+const os = require('os')
+const fs = require('fs')
+const stream = require('stream')
+const path = require('path')
+const unzipper = require('unzipper')
+const version = require('./package.json').version
 
 // var darwinSha256 = ''
 // var linuxSha256 = ''
 
-var url
+let url
 // var sha256
-var binary = `moonbeam-v${version}-macos-11.0`
+const binary = `moonbeam-v${version}-macos-11.0`
 
 switch (os.platform()) {
 //   case 'win32':
@@ -38,9 +38,9 @@ get(url, function (err, res) {
     process.exit(1)
   }
 
-  var tmpDir = path.resolve(__dirname, './moonbeam.tmp')
-  var unzipr = unzipper.Extract({ path: tmpDir })
-//   var hash = crypto.createHash('sha256')
+  const tmpDir = path.resolve(__dirname, `./${binary}.tmp`)
+  const unzipr = unzipper.Extract({ path: tmpDir })
+  //   var hash = crypto.createHash('sha256')
 
   stream.pipeline(res, unzipr, function (err) {
     if (err) {
@@ -48,13 +48,14 @@ get(url, function (err, res) {
       process.exit(1)
     }
 
-    if (hash.digest('hex') !== sha256) {
-      console.error('SHA-256 checksum mismatch')
-      process.exit(1)
-    }
+    // if (hash.digest('hex') !== sha256) {
+    //   console.error('SHA-256 checksum mismatch')
+    //   process.exit(1)
+    // }
 
-    fs.renameSync(path.join(tmpDir, binary), path.resolve(__dirname, binary))
-    fs.chmodSync(path.resolve(__dirname, binary), 0o777)
+    fs.renameSync(path.join(tmpDir, binary), path.resolve(__dirname, "moonbeam"))
+    fs.chmodSync(path.resolve(__dirname, "moonbeam"), 0o777)
+    fs.rmdirSync(tmpDir)
   })
 
 //   res.on('data', chunk => hash.update(chunk))
