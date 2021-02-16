1e90f3ad5165736a5373326d16c5f5428aa64f3c
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-16 16:28:56 +0100

    impl: install script slimd down

--- install.js
+++ install.js
@@ -1,5 +1,4 @@
 const get = require('simple-get')
-// var crypto = require('crypto')
 const os = require('os')
 const fs = require('fs')
 const stream = require('stream')
@@ -7,49 +6,30 @@ const path = require('path')
 const gunzip = require('gunzip-maybe')
 const version = `v${process.env.MOONBEAM_TAG || require('./package.json').version}`
 
-// var darwinSha256 = ''
-// var linuxSha256 = ''
-
-let url
-// var sha256
-let binary = `moonbeam-${version}-`
-
-switch (os.platform()) {
-  // case 'win32':
-  //   url = ''
-  //   sha256 = win32Sha256
-  //   break
-  // lately fails with "Error: cmake 3.19.4 is already installed"
-  // case 'darwin':
-  //   binary += 'macos-11.0'
-  //   url = `https://github.com/nugget-digital/moonbeam-binary/releases/download/${version}/${binary}.gz`
-  //   // sha256 = darwinSha256
-  //   break
-  case 'linux':
-    binary += 'ubuntu-20.04'
-    url = `https://github.com/nugget-digital/moonbeam-binary/releases/download/${version}/${binary}.gz`
-    // sha256 = linuxSha256
-    break
-  default:
-    console.error('No moonbeam binary found for:', os.platform())
-    process.exit(1)
+function panic (msg) {
+  console.error(msg)
+  process.exit(1)
 }
 
-console.log('[DEBUG] url', url)
+let url
+
+switch (os.platform()) {
+  case 'linux':
+    url = `https://github.com/nugget-digital/moonbeam-binary/releases/download/${version}/moonbeam-${version}-ubuntu-20.04.gz`
+    break
+  default:
+    panic('no moonbeam binary found for ' + os.platform())
+}
+
+console.debug('url', url)
 
 get(url, function (err, res) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
+  if (err) panic(err.message)
 
   const moonbeam = path.resolve(__dirname, 'moonbeam')
 
   stream.pipeline(res, gunzip(), fs.createWriteStream(moonbeam), function (err) {
-    if (err) {
-      console.error(`Download failed - ${err.message}`)
-      process.exit(1)
-    }
+    if (err) panic(err.message)
 
     fs.chmodSync(moonbeam, 0o777)
   })
