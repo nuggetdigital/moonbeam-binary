5819bc9bd7f7f2b2db802f3c527b8a67074dfbf5
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-22 12:35:13 +0100

    impl: sha256sum verification in da install script

--- install.js
+++ install.js
@@ -1,4 +1,5 @@
 const get = require('simple-get')
+const crypto = require('crypto')
 const os = require('os')
 const fs = require('fs')
 const stream = require('stream')
@@ -6,31 +7,54 @@ const path = require('path')
 const gunzip = require('gunzip-maybe')
 const version = `v${process.env.MOONBEAM_TAG || require('./package.json').version}`
 
-function panic (msg) {
-  console.error(msg)
+function panic (err) {
+  err && console.error(err.message || err)
   process.exit(1)
 }
 
 let url
+let sha
 
 switch (os.platform()) {
   case 'linux':
     url = `https://github.com/nugget-digital/moonbeam-binary/releases/download/${version}/moonbeam-${version}-ubuntu-20.04.gz`
+    sha = `https://github.com/nugget-digital/moonbeam-binary/releases/download/${version}/moonbeam-${version}-ubuntu-20.04.gz.sha256sum`
     break
   default:
     panic('no moonbeam binary found for ' + os.platform())
 }
 
-console.debug('url', url)
+get(sha, function (err, r) {
+  if (err) panic(err)
 
-get(url, function (err, res) {
-  if (err) panic(err.message)
+  const chunks = []
 
-  const moonbeam = path.resolve(__dirname, 'moonbeam')
+  r.on('error', panic)
 
-  stream.pipeline(res, gunzip(), fs.createWriteStream(moonbeam), function (err) {
-    if (err) panic(err.message)
+  r.on('data', function (chunk) {
+    chunks.push(chunk)
+  })
 
-    fs.chmodSync(moonbeam, 0o777)
+  r.on('end', function () {
+    const expected = Buffer.concat(chunks).toString('hex')
+
+    get(url, function (err, res) {
+      if (err) panic(err)
+
+      const hash = crypto.createHash('sha256')
+      const moonbeam = path.resolve(__dirname, 'moonbeam')
+
+      res.on('data', function (chunk) {
+        hash.update(chunk)
+      })
+
+      stream.pipeline(res, gunzip(), fs.createWriteStream(moonbeam), function (err) {
+        if (err) panic(err)
+
+        if (hash.digest('hex') !== expected) panic('SHA-256 checksum mismatch')
+
+        fs.chmodSync(moonbeam, 0o777)
+      })
+    })
   })
 })
